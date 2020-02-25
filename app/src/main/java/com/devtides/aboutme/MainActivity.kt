4295f0a2f75b75b43dc083417e7e31a55fa7dc16
Marcelo <dantasmr@yahoo.com.br>
2020-02-24 22:26:53 -0300

    first commit

---
+++ app/src/main/java/com/devtides/aboutme/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.devtides.aboutme
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.devtides.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+
+
+
+    }
+
+    private fun addNickname(it: View?) {
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(it?.windowToken, 0)
+
+        binding.apply {
+            nicknameText.text = nicknameEdit.text
+            nicknameText.visibility = View.VISIBLE
+            doneButton.visibility = View.GONE
+            nicknameEdit.setText("")
+        }
+
+
+
+    }
+}
